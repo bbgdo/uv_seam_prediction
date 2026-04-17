a3a686b705dc2b882b6679cb4487bfff07e233d7
chucho <bohdan.tarverdiiev@gmail.com>
2026-04-17 13:24:25 +0300

    tweak: gatv2 params

--- models/baselines/gatv2.py
+++ models/baselines/gatv2.py
@@ -3,10 +3,19 @@ from models.gatv2.model import DualGATv2
 
 MODEL_CLASS = DualGATv2
 DISPLAY_NAME = 'GATv2'
+# DEFAULT_CONFIG_OVERRIDES = {
+#     'hidden_size': 32,
+#     'lr': 5e-4,
+#     'heads': 4,
+#     'num_layers': 3,
+#     'dropout': 0.3,
+# }
+
 DEFAULT_CONFIG_OVERRIDES = {
-    'hidden_size': 32,
-    'lr': 5e-4,
+    'hidden_size': 64,
+    'lr': 3e-4,
     'heads': 4,
-    'num_layers': 3,
-    'dropout': 0.3,
+    'num_layers': 4,
+    'dropout': 0.2,
+    'patience': 50
 }
